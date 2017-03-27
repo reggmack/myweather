16e9cab5f4763ee52dd6c774d5d6bec025336756
reggmack <thegraphixninja@gmail.com>
2017-03-27 17:18:36 -0500

    Autocomplete API
    Autocomplete API

--- src/pages/weather/weather.ts
+++ src/pages/weather/weather.ts
@@ -1,25 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { WeatherService } from '../../app/services/weather.service';
+import {WeatherService} from '../../app/services/weather.service';
 
 @Component({
   selector: 'weather',
   templateUrl: 'weather.html'
 })
 export class WeatherPage {
-city: String;
-state: String;
-weather: any;
-  
+  city: String;
+  state: String;
+  weather: any;
+  searchStr:String;
+  results: any;
+
   constructor(public navCtrl: NavController, private weatherService:WeatherService) {
     this.city = 'Boston';
     this.state = 'MA';
   }
 
   ngOnInit(){
-      this.weatherService.getWeather(this.city, this.state)
+    this.weatherService.getWeather(this.city, this.state)
       .subscribe(weather => {
-       this.weather = weather.current_observation;
+        this.weather = weather.current_observation;
+      });
+  }
+
+  getQuery(){
+    this.weatherService.searchCities(this.searchStr)
+      .subscribe(res => {
+        console.log(res);
+        //this.results = res.RESULTS;
       });
   }
 
