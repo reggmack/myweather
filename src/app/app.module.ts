17923198ec1f2db8c8570f7cb6444a5c3b9b7ffc
reggmack <thegraphixninja@gmail.com>
2017-03-27 14:36:07 -0500

    Set up Pages
    Set up Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { WeatherPage } from '../pages/weather/weather';
+import { SettingsPage } from '../pages/settings/settings';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -12,9 +11,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    WeatherPage,
+    SettingsPage,
     TabsPage
   ],
   imports: [
@@ -23,9 +21,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    WeatherPage,
+    SettingsPage,
     TabsPage
   ],
   providers: [
---
+++ src/pages/weather/weather.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'weather',
+  templateUrl: 'weather.html'
+})
+export class WeatherPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
